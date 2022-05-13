d673f3d04d7c4a7c56af05dc570c943d843863b0
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-05-13 23:39:33 +0700

    update base project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="font-bold container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur dolor dolore
+      doloribus ipsum officia officiis praesentium quasi quidem veritatis! Atque consequatur culpa
+      distinctio error nam porro possimus tempore voluptas?
+    </div>
+  );
+};
+
+export default App;
